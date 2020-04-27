a50ae0c033c768bc5bf61b9b24683619b7174e3c
patruong <ptruong@kth.se>
2020-04-27 17:20:02 +0200

    updated all scripts

--- data_wrangling.py
+++ data_wrangling.py
@@ -1,170 +1,75 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Apr 22 11:08:27 2020
+Created on Mon Apr 27 14:14:50 2020
 
 @author: ptruong
 """
 
-import os
 
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-# filename = "20200306_AminoAcid_Pro_1_fit.csv"
-# MS_folder =  "MS"
-# MS = pd.read_csv(MS_folder + "/" + filename )    
-# MSMS_folder = "MSMS"
-# MSMS = pd.read_csv(MSMS_folder + "/" + filename )    
+
+def get_formatted_dataframe(df):
+    df_new = df.loc[:, ["seqNum", "peak", "isoratio", "ion", "file", "scan", "n"]]
+    df_new.isoratio = df_new.isoratio / df_new.n
+    df_new.drop("n", axis = 1, inplace = True)
+    return df_new
 
 
-
-def get_file_list(folder):
+def transform_df_boxplot(df, peak, x = "MS", y = "MSMS_HCD_125"):
     """
-    Gets files in folder.
+    Takes input from get_formatted_dataframe and gives df_boxplot output format.
+    
+    peak - the peak from "peak" column of get_formatted_dataframe. We plot boxplots for each peak.
+        x - one scan type (x-axis)
+        y - second scan type (y-axis)
     """
-    folder = folder
-    files = os.listdir(folder)
-    res = []
-    for f in files:
-        if f[0] == "2":
-            res.append(f)
-    return res
-
-
-# sålla bort onödiga aminosyror från MS - dataframe
-def filter_on_amino_acids(file, amino_acids = ["P", "Hyp"]):
-    """
-    Function takes file name to find which amino acid is prevalent in sample
-    and filters on that.
+    print("NOTE TO SELF: Check if data is empty - future version need error handler!")
+    boxplot_cols = [
+      "x_count", "x_mean", "x_std","x_min", "x_lower", "x_middle", "x_upper", "x_max",  
+      "y_count", "y_mean", "y_std","y_min", "y_lower", "y_middle", "y_upper", "y_max",
+      "category"
+    ]
     
-    file = file name
-    amino_acids all amino acids in file folder that was checked for.
-    """
-    file = file
-    amino_acids = amino_acids
+    df_boxplot = pd.DataFrame(data = None, columns = boxplot_cols)
     
-    df = pd.read_csv(file)
-    file = file.replace("Pro", "P") #Replace Pro in filenames with P.
-    file_name_splitted = file.split("_")
-    for amino_acid in amino_acids:
-        if amino_acid in file_name_splitted:
-            df_res = df[df.ion == amino_acid]
-    return df_res # Might/should cause error if no matching ion!
-
-def get_filtered_data(ms_file, scan_type = "MS", amino_acids = ["P", "Hyp"]):
-    """
-    Function filters the correct ion from the ms and msms files.
+    for category in df["ion"].unique():
+        df_category = df[df["ion"] == category]
+        df_category = df_category[df_category["peak"] == peak]
+        
+        var_x = df_category[df_category["scan"] == x].isoratio.describe()
+        var_y = df_category[df_category["scan"] == y].isoratio.describe()
+        
+        data_boxplot = np.concatenate((var_x, var_y))
+        data_boxplot = np.append(data_boxplot, category)
     
-    Function also corrects the seqNum for MSMS scan so it has matching 
-    seqNum to its MS counterpart.
-    """
-    scan_type = scan_type
-    amino_acids = amino_acids
-    file = scan_type + "/" + ms_file
-    df = filter_on_amino_acids(file, amino_acids)
-    df = df.loc[:, ["seqNum", "peak", "isoratio", "ion"]] # columns to filter on
-    
-    if scan_type == "MSMS":    
-        df.seqNum = (df.seqNum - 1)
-    
-    filename = ms_file
-    filename_col = np.array([filename for i  in range(len(df))])
-    df["filename"] = filename_col
-    
-    scan_type_col = np.array([scan_type for i in range(len(df))])
-    df["scan_type"] = scan_type_col
-    return df
-
-# check diff between two lists
-def check_difference_between_lists(list1, list2):
-    return list(set(list1)-set(list2))
-
-def get_formatted_dataframe(amino_acids = ["P", "Hyp"]):
-    """
-    Function formats data so that we can work with the isoratios.
-    """
-    amino_acids = amino_acids # Specify amino acids
-    folders = ["MS", "MSMS"] # Specify folder
-    
-    
-    ms_files = get_file_list("MS")
-    msms_files = ms_files = get_file_list("MSMS")    
-    
-    if not check_difference_between_lists(ms_files, msms_files):
-        print("Files in MS and MSMS folders are matching!")
-    else:
-        print("WARNING: files in MS and MSMS folders are not same!")
-    
-    print("Loading and adding files...")
-    df = pd.DataFrame(data=None, columns = ["seqNum", "peak", "isoratio", "ion", "filename", "scan_type"])
-    for ms_type in folders:
-        for file in ms_files:
-            df_ms = get_filtered_data(ms_file = file, scan_type = ms_type, amino_acids = amino_acids)
-            df = df.append(df_ms)
-            print(ms_type + "/" + file)
-    print("Done!")
-    return df
-
-#if __name__ == "__main__":
-
-    
-df = get_formatted_dataframe(amino_acids = ["P", "Hyp"])
+        df_boxplot_values = pd.DataFrame(data = [data_boxplot],
+                                         columns = boxplot_cols)
+        df_boxplot = df_boxplot.append(df_boxplot_values)
+    return df_boxplot
 
 
-############
-# Plotting #
-############
+df = pd.read_csv("tSIM experiment.csv")
 
-# Load plotnine.
-from plotnine import *
-
-# slice my data
-z
+df_formatted = get_formatted_dataframe(df)
 
 
+x_lab = "MS"
+y_lab = "MSMS_HCD_125"
+iso = "13C"
 
-
-
-
-##############################
-# DOUBLE BOX PLOT FORMATTING #
-##############################
-
-df.to_csv("aminos.csv")
-# Format the data to iris format
-
-boxplot_cols = [
-  "x_count", "x_mean", "x_std","x_min", "x_lower", "x_middle", "x_upper", "x_max",  
-  "y_count", "y_mean", "y_std","y_min", "y_lower", "y_middle", "y_upper", "y_max",
-  "category"
-]
-
-df_boxplot = pd.DataFrame(data = None, columns = boxplot_cols)
-
-category = "setosa" # ions P, Hyp and V 
-
-var_x = df_category["sepalLength"].describe()
-var_y = df_category["sepalWidth"].describe()
-
-df_ion = df[df.peak == "13C"]
-df_MS = df_ion[df_ion.scan_type == "MS"]
-df_MSMS = df_ion[df_ion.scan_type == "MSMS"]
-
-for category in df["species"].unique():
-    df_category = df[df["species"] == category]
-
-    var_x = df_category["sepalLength"].describe()
-    var_y = df_category["sepalWidth"].describe()
-    data_boxplot = np.concatenate((var_x, var_y))
-    data_boxplot = np.append(data_boxplot, category)
-    
-    df_boxplot_values = pd.DataFrame(data = [data_boxplot],
-                                     columns = boxplot_cols)
-    df_boxplot = df_boxplot.append(df_boxplot_values)
-
+for i in df_formatted.peak.unique(): 
+    if i != "0":
+        iso = i
+        df_boxplot = transform_df_boxplot(df_formatted, 
+                                          peak = iso,
+                                          x = x_lab,
+                                          y = y_lab)        
+        df_boxplot.to_csv("results/" + iso + "__" + x_lab + "__" + y_lab + ".csv", sep = ";", index = False)
 
 
 
--- tutorial/r_ggplot.R
+++ tutorial/r_ggplot.R
@@ -1,30 +1,78 @@
 rm(list=ls())
 library(tidyverse)
 library("ggplot2")
+require(gridExtra)
 
-df <- read.csv("TEST.csv", sep = ";", header = TRUE)
+df <- read.csv("TEST_ion.csv", sep = ";", header = TRUE)
 
-ggplot(df, aes(fill = category, color = category)) +
-  
-  # 2D box defined by the Q1 & Q3 values in each dimension, with outline
-  geom_rect(aes(xmin = x_lower, xmax = x_upper, ymin = y_lower, ymax = y_upper), alpha = 0.3) +
-  geom_rect(aes(xmin = x_lower, xmax = x_upper, ymin = y_lower, ymax = y_upper), 
-            color = "black", fill = NA) +
-  
-  # whiskers for x-axis dimension with ends
-  geom_segment(aes(x = x_min, y = y_middle, xend = x_max, yend = y_middle)) + #whiskers
-  geom_segment(aes(x = x_min, y = y_lower, xend = x_min, yend = y_upper)) + #lower end
-  geom_segment(aes(x = x_max, y = y_lower, xend = x_max, yend = y_upper)) + #upper end
-  
-  # whiskers for y-axis dimension with ends
-  geom_segment(aes(x = x_middle, y = y_min, xend = x_middle, yend = y_max)) + #whiskers
-  geom_segment(aes(x = x_lower, y = y_min, xend = x_upper, yend = y_min)) + #lower end
-  geom_segment(aes(x = x_lower, y = y_max, xend = x_upper, yend = y_max)) + #upper end
-  
-  # outliers
-  #geom_point(data = df.outliers, aes(x = x.outliers, y = y.middle), size = 3, shape = 1) + # x-direction
-  #geom_point(data = df.outliers, aes(x = x.middle, y = y.outliers), size = 3, shape = 1) + # y-direction
-  
-  xlab("Sepal.Length") + ylab("Sepal.Width") +
-  coord_cartesian(xlim = c(4, 8), ylim = c(2, 4.5)) +
-  theme_classic()
+
+double_box_plot <- function(df, title = "title", x_lab = "MS", y_lab = "MSMS") {
+  ggplot(df, aes(fill = category, color = category)) +
+    
+    # 2D box defined by the Q1 & Q3 values in each dimension, with outline
+    geom_rect(aes(xmin = x_lower, xmax = x_upper, ymin = y_lower, ymax = y_upper), alpha = 0.3) +
+    geom_rect(aes(xmin = x_lower, xmax = x_upper, ymin = y_lower, ymax = y_upper), 
+              color = "black", fill = NA) +
+    
+    # whiskers for x-axis dimension with ends
+    geom_segment(aes(x = x_min, y = y_middle, xend = x_max, yend = y_middle)) + #whiskers
+    geom_segment(aes(x = x_min, y = y_lower, xend = x_min, yend = y_upper)) + #lower end
+    geom_segment(aes(x = x_max, y = y_lower, xend = x_max, yend = y_upper)) + #upper end
+    
+    # whiskers for y-axis dimension with ends
+    geom_segment(aes(x = x_middle, y = y_min, xend = x_middle, yend = y_max)) + #whiskers
+    geom_segment(aes(x = x_lower, y = y_min, xend = x_upper, yend = y_min)) + #lower end
+    geom_segment(aes(x = x_lower, y = y_max, xend = x_upper, yend = y_max)) + #upper end
+    
+    # outliers
+    #geom_point(data = df.outliers, aes(x = x.outliers, y = y.middle), size = 3, shape = 1) + # x-direction
+    #geom_point(data = df.outliers, aes(x = x.middle, y = y.outliers), size = 3, shape = 1) + # y-direction
+    
+    xlab(x_lab) + ylab(y_lab) +
+    ggtitle(title) +
+    #coord_cartesian(xlim = c(0, 0.1), ylim = c(0, 0.1)) +
+    theme_classic()
+}
+
+df1 <- read.csv("TEST_13C.csv", sep = ";", header = TRUE)
+df2 <- read.csv("TEST_15N.csv", sep = ";", header = TRUE)
+df3 <- read.csv("TEST_2H.csv", sep = ";", header = TRUE)
+df4 <- read.csv("TEST_18O.csv", sep = ";", header = TRUE)
+
+plot1 <- double_box_plot(df1, title = "12C/13C")
+plot2 <- double_box_plot(df2, title = "14N/15N")
+plot3 <- double_box_plot(df3, title = "1H/2H")
+plot4 <- double_box_plot(df4, title = "16O/18O")
+
+grid.arrange(plot1, plot2, plot3, plot4, ncol=2)
+
+plots <- list()
+
+for (i in list.files()){
+  splitted <- unlist(strsplit(i, "\\, |\\__|, |\\.| "))
+  title <- splitted[1]
+  x_lab <- splitted[2]
+  y_lab <- splitted[3]
+  df <- read.csv(i, sep = ";", header = TRUE)
+  plots[[i]] <- double_box_plot(df, title = title, x_lab = x_lab, y_lab = y_lab)
+}
+do.call(grid.arrange, plots)
+
+
+
+set.seed(154)
+D <- data.frame(
+  x1 = runif(100),
+  x2 = rnorm(100)
+)
+
+library(ggplot2)
+plots <- list()
+for(nm in names(D)) {
+  plots[[nm]] <- ggplot(data=D) + geom_density(aes_string(x=nm))
+}
+
+print(plots[["x1"]])
+print(plots[["x2"]])
+do.call(grid.arrange, plots)
+                            
