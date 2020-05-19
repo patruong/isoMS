8a3f13722ad052e30b9bcb21c0f4d58e757fbcbb
patruong <ptruong@kth.se>
2020-05-19 16:57:47 +0200

    updated

--- data_wrangling_firstExp.py
+++ data_wrangling_firstExp.py
@@ -3,7 +3,9 @@
 """
 Created on Mon May 18 15:15:38 2020
 
-@author: esynergetics
+@author: ptruong
+
+File should used on folder with scan folders...
 """
 
 import pandas as pd
@@ -27,11 +29,48 @@ def read_and_transform_file(file, scan):
     return df_vals
 
 
+def filter_df_on_relevant_ion(file, scan):
+    df = read_and_transform_file(file, scan)
+    target_ion = file.split("/")[1].split("_")[5][0]
+    print(target_ion)
+    df_filtered = df[df["ion"] == target_ion]
+    return df_filtered
+
 df = read_and_transform_file(file, scan)
+df = filter_df_on_relevant_ion(df)
 
-target_ion = file.split("_")[5][0]
 
-df[df["ion"] == target_ion]
+import os
+
+scans = os.listdir()
+
+df  =
+for scan in scans:
+    files = os.listdir(scan)
+    for file in files:
+        file_dir = (scan + "/" + file)
+        print(file_dir)
+        df = filter_df_on_relevant_ion(file_dir, file_dir.split("/")[0])
+        df = df.append(df)
+        print(len(df))
+
+
+
+test1 = "MS/20200402_HCD0_HCD50_60K_AGC5e4_Hyp_1_at125_tSIM_fit.csv"
+test2 = "MS/20200402_HCD0_HCD50_60K_AGC5e4_Pro_3_at125_tSIM_fit.csv"
+
+
+df1 = filter_df_on_relevant_ion(test1, test1.split("/")[0])
+
+df2 = filter_df_on_relevant_ion(test2, test2.split("/")[0])
+
+df_t = df1.append(df2)
+d
+
+df = df1
+
+
+
 
 
 
